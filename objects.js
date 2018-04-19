3ffecdba9b4597fa8ccfd6c00e51ee395d9b3fa6
Hanna S <hanna.soloman@gmail.com>
2018-04-19 12:23:57 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,11 +4,11 @@ var playlist = {
 };
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist[artistName] = songTitle
-  return playlist
+  playlist[artistName] = songTitle;
+  return playlist;
 }
 
 function removeFromPlaylist(playlist, artistName){
   delete playlist[artistName];
-  return playlist
+  return playlist;
 }
